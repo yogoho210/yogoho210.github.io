56787316dda0038abd9358db23075c4ee54aa274
yogoho210 <haeyun210@naver.com>
2019-12-21 09:55:50 +0900

    Add files via upload

---
+++ precache-manifest.7f46df4fb5c7c92a3f4caf7d7b186570.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35a615178cfcf164bbf2",
+    "url": "/css/app.ecb7d889.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "c614947f5ace37a78b4922e07285b8af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35a615178cfcf164bbf2",
+    "url": "/js/app.e74bdcd6.js"
+  },
+  {
+    "revision": "4bec52cbb1279ef7a4a8",
+    "url": "/js/chunk-vendors.cf491c02.js"
+  },
+  {
+    "revision": "af4a8287fe9ccfd409c96c143f922eca",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
